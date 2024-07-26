5f20a10acaed51ef15b76afadf839c5e5ea8a4b6
Lissone <leonardo.lissonez@gmail.com>
2024-07-26 15:10:14 -0300

    feat: install realmdb

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import { ThemeProvider } from 'styled-components/native';
+import { StatusBar } from 'react-native';
+import { AppProvider, UserProvider } from '@realm/react';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import theme from './src/theme';
 
+import { REALM_APP_ID } from '@env';
+
+import { Home } from './src/screens/Home';
 import { SignIn } from './src/screens/SignIn';
 import { Loading } from './src/components/Loading';
-import { StatusBar } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -20,13 +24,19 @@ export default function App() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
+    <AppProvider id={REALM_APP_ID}>
+      <ThemeProvider theme={theme}>
+
       <StatusBar 
         barStyle="light-content" 
         backgroundColor="transparent" 
         translucent 
       />
-      <SignIn />
+
+      <UserProvider fallback={SignIn}>
+        <Home />
+      </UserProvider>
     </ThemeProvider>
+    </AppProvider>
   );
 }
