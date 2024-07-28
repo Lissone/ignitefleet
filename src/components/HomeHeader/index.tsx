509862a10a5eb7629c641bb8bb9485400e7cd2ff
Lissone <leonardo.lissonez@gmail.com>
2024-07-28 13:14:48 -0300

    feat(components): home header finished

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,19 +1,39 @@
-import React from 'react';
-import { Container, Greeting, Message, Name } from './styles';
+import { TouchableOpacity } from 'react-native'
+import { Power } from 'phosphor-react-native'
+import { useApp, useUser } from '@realm/react';
+
+import { Container, Greeting, Message, Name, Picture } from './styles';
+
+import theme from '../../theme';
 
 export function HomeHeader() {
+  const user = useUser();
+  const app = useApp();
+
+  function handleLogOut() {
+    app.currentUser?.logOut();
+  }
+  
   return (
     <Container>
+      <Picture 
+        source={{ uri: user?.profile.pictureUrl }}
+        placeholder='L184i9ofbHof00ayjsay~qj[ayj@'
+      />
+
       <Greeting>
         <Message>
           Olá
         </Message>
 
         <Name>
-          Rodrigo
+          {user?.profile.name}
         </Name>
       </Greeting>
 
+      <TouchableOpacity activeOpacity={0.7} onPress={handleLogOut}>
+        <Power size={32} color={theme.COLORS.GRAY_400} />
+      </TouchableOpacity>
     </Container>
   );
 }
